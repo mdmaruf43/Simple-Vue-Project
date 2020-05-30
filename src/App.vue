c1388cfd2609c323ae2c542e03a6519bc06f4db2
mdmaruf43 <marufahmedcse76@gmail.com>
2020-05-30 20:58:31 +0600

    first vue project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo />
+    <Todo v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/layout/Header'
+import Todo from './components/Todo'
+import AddTodo from './components/AddTodo'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Todo,
+    AddTodo
+  },
+  data () {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Todo One',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Todo Two',
+          completed: true
+        },
+        {
+          id: 3,
+          title: 'Todo Three',
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo (id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
+
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <form>
+      <input type="text" v-model="title" name="title" placeholder="Add Todo...">
+      <input type="submit" value="Submit" class="btn">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddTodo',
+  data () {
+    return {
+      title: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
